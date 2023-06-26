11b37595a1a21314a89713a35f895bed40faadd6
pvwnthem <pvwn.them@gmail.com>
2023-06-26 19:16:06 -0400

    remove shitty next.js font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,8 @@ import './globals.css'
 
 import React from "react";
 
-import { Inter } from 'next/font/google'
 
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'tf.2x1.dev',
@@ -19,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
