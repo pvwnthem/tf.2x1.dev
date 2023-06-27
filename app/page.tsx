c571c38a01168ae3fafc42de8e17eed8c434e9f4
pvwnthem <pvwn.them@gmail.com>
2023-06-26 20:03:27 -0400

    update layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,10 @@ import React from "react";
 
 export default function Home() {
   return (
-   <div className="w-full h-screen">
+   <div className="bg-background">
     <Navbar />
     <Background />
    </div>
+   
   )
 }
