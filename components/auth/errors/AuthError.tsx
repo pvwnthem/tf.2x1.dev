087156f28a02a4a08cd52a1150ccdd2cbe615e8e
pvwnthem <pvwn.them@gmail.com>
2023-06-27 12:31:27 -0400

    add login button to autherror

--- components/auth/errors/AuthError.tsx
+++ components/auth/errors/AuthError.tsx
@@ -7,6 +7,7 @@ export default function AuthError () {
             <div className="w-1/2 flex flex-col">
                 <h1 className="text-wave-300 text-center text-7xl mt-48">oops!</h1>
                 <h2 className="text-wave-400 text-center text-3xl mt-12">please login to access this page!</h2>
+                <button className="bg-wave-400 px-8 py-2 text-lg mx-auto rounded-lg mt-8" onClick={() => window.location.replace("/login")}>login</button>
             </div>
         </div>
     )
