cb047bdcf84de7a978a9f68399a8f7263e7f8fad
pvwnthem <pvwn.them@gmail.com>
2023-06-26 20:01:27 -0400

    start frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { Navbar } from "@components/navigation/navbar";
+import Background from "@components/svg/background";
 import React from "react";
 
 export default function Home() {
   return (
-   <>
+   <div className="w-full h-screen">
     <Navbar />
-   </>
+    <Background />
+   </div>
   )
 }
--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Logo from '@components/svg/logo';
 import Link from 'next/link';
 import React, { useState } from 'react';
 
@@ -13,72 +14,40 @@ export const Navbar = () => {
 
   return (
     <div className="navbar">
-      <nav className="bg-white shadow-sm">
+      <nav className="bg-background shadow-sm">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex-shrink-0">
               <Link href="/" className="flex items-center">
-                <svg
-                  className="h-6 w-6 text-green-600 mr-2"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M12 1L2 7V17L12 23L22 17V7L12 1Z"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M12 22V11"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M7 13H17"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M7 17H17"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-                <span className="font-medium text-green-600 text-lg">2x1.dev</span>
+              <Logo />
+                
+                <span className="font-medium text-wave-100 text-lg">tf.2x1.dev</span>
               </Link>
             </div>
             <div className="hidden lg:flex space-x-4 lg:items-center lg:justify-end lg:flex-1">
               <button
                 onClick={() => {scroll('home')}}
-                className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-green-600 hover:bg-green-100 focus:outline-none focus:text-green-600 focus:bg-green-100">
+                className="px-3 py-2 rounded-md text-sm font-medium text-wave-100 hover:text-wave-400 focus:outline-none focus:text-wave-400">
                 Home
               </button>
               <button
                 onClick={() => {scroll('about')}}
-                className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-green-600 hover:bg-green-100 focus:outline-none focus:text-green-600 focus:bg-green-100">
+                className="px-3 py-2 rounded-md text-sm font-medium text-wave-100 hover:text-wave-400 focus:outline-none focus:text-wave-400">
                 About
               </button>
               <button
                 onClick={() => {scroll('projects')}}
-                className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-green-600 hover:bg-green-100 focus:outline-none focus:text-green-600 focus:bg-green-100">
+                className="px-3 py-2 rounded-md text-sm font-medium text-wave-100 hover:text-wave-400 focus:outline-none focus:text-wave-400">
                 Projects
               </button>
-              <button  onClick={() => {scroll('contact')}} className=" px-3 py-2 bg-green-600 hover:bg-green-700 rounded-md text-white">
+              <button  onClick={() => {scroll('contact')}} className=" px-3 py-2 bg-wave-300 hover:bg-wave-400 rounded-md text-white">
                 Contact
               </button>
             </div>
             <div className="flex lg:hidden">
               <button
                 type="button"
-                className="inline-flex items-center justify-center p-2 rounded-md text-green-600 hover:text-green-700 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-green-600"
+                className="inline-flex items-center justify-center p-2 rounded-md text-wave-100 hover:text-wave-400 "
                 aria-expanded={isMobileMenuOpen}
                 aria-haspopup="true"
                 onClick={() => {
@@ -121,20 +90,20 @@ export const Navbar = () => {
           <div className="px-2 pt-2 pb-3 space-y-2">
             <button
               onClick={() => {scroll('home')}}
-              className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-green-600 hover:bg-green-100 focus:outline-none focus:text-green-600 focus:bg-green-100">
+              className="block px-3 py-2 rounded-md text-base font-medium text-wave-100 hover:text-wave-400  focus:outline-none focus:text-wave-400">
               Home
             </button>
             <button
               onClick={() => {scroll('about')}}
-              className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-green-600 hover:bg-green-100 focus:outline-none focus:text-green-600 focus:bg-green-100">
+              className="block px-3 py-2 rounded-md text-base font-medium text-wave-100 hover:text-wave-400 focus:outline-none focus:text-wave-400">
               About
             </button>
             <button
               onClick={() => {scroll('projects')}}
-              className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-green-600 hover:bg-green-100 focus:outline-none focus:text-green-600 focus:bg-green-100">
+              className="block px-3 py-2 rounded-md text-base font-medium text-wave-100 hover:text-wave-400 focus:outline-none focus:text-wave-400">
               Projects
             </button>
-            <button onClick={() => {scroll('contact')}} className=" px-3 py-2 w-full hover:bg-green-700 bg-green-600 rounded-md text-white">
+            <button onClick={() => {scroll('contact')}} className=" px-3 py-2 w-full bg-wave-300 hover:bg-wave-400 rounded-md text-white">
               Contact
             </button>
           </div>
