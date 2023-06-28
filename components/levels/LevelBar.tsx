d67aa056873cfbcaa4836feccba6d326335f829b
pvwnthem <pvwn.them@gmail.com>
2023-06-28 16:09:09 -0400

    add tests back to levelbar

--- components/levels/LevelBar.tsx
+++ components/levels/LevelBar.tsx
@@ -8,12 +8,34 @@ import { connect } from "@lib/mongodb";
 export default function LevelBar ( props : { session : any } ) {
     
     const { level, xp, id } = props.session.data.user
+    const { update } = props.session
+
+
+    function addTest () {
+        addXP(id, 100).then((user) => {
+            update({
+                ...props.session,
+                data: {
+                    ...props.session.data,
+                    user
+                }
+            })
+            console.log(user, level, xp, id)
+        })
+    }
+
+    function l () {
+        console.log(props.session)
+    }
 
     return (
         <>
             <div className="flex flex-col items-center justify-center w-1/2">
-                <img className="md:w-1/5 w-1/4" src={levels[level].badge}></img>
+                <img className="" src={levels[level].badge}></img>
                 <h2 className="text-center mt-2 text-wave-200">{xp} xp / {levels[level + 1].xpRequired} xp</h2>
+
+                <button onClick={addTest}>add 100 xp</button>
+                <button onClick={l}>log</button>
             </div>
         </>
     )
