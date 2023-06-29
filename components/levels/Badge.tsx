de784e3db7a80f1756c9676f9f955d3f23b5dc06
pvwnthem <pvwn.them@gmail.com>
2023-06-28 21:40:37 -0400

    move badge to component

---
+++ components/levels/Badge.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+'use client'
+import { levels } from '@constants/levels';
+import React from 'react';
+
+export default function Badge ( props: { session: any } ) {
+
+    const { level, xp, title } = props.session.data.user;
+    
+    return (
+        <>
+            <img alt="badge" className="" src={levels[level].badge} />
+            <h1 className="mt-1  text-wave-300">{title}</h1>
+            <h2 className="text-center mt-1 text-wave-200">
+                {xp} xp / {levels[level + 1].xpRequired} xp
+            </h2>
+        </>
+    )
+
+}
--- components/levels/LevelBar.tsx
+++ components/levels/LevelBar.tsx
@@ -1,43 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import React, { useEffect } from "react";
-import { levels } from "@constants/levels";
-import { addXP } from "@services/levels.service";
+import React from "react";
+import Badge from "./Badge";
 
 export default function LevelBar(props: { session: any }) {
-    const { level, xp, id, title } = props.session.data.user;
-    const { update } = props.session;
-
-    function addTest() {
-        addXP(id, 100).then((user) => {
-            update({
-                ...props.session,
-                data: {
-                    ...props.session.data,
-                    user,
-                },
-            });
-            console.log(user, level, xp, id);
-        });
-    }
-
-    function l() {
-        console.log(props.session);
-    }
 
     return (
         <>
             <div className="flex flex-col items-center justify-center w-1/2">
-                {/* BEGIN: keeping this part when redoing styling also move it to its own component soon */}
-                <img alt="badge" className="" src={levels[level].badge}></img>
-                <h1 className="mt-1  text-wave-300">{title}</h1>
-                {/* END: keep */}
-                <h2 className="text-center mt-1 text-wave-200">
-                    {xp} xp / {levels[level + 1].xpRequired} xp
-                </h2>
 
-                <button onClick={addTest}>add 100 xp</button>
-                <button onClick={l}>log</button>
+                <Badge session={props.session} />
+
             </div>
         </>
     );
