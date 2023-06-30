e910743d4f21220735f8f3b40d23b1320c728ecd
pvwnthem <pvwn.them@gmail.com>
2023-06-30 20:08:33 +0000

    center description text

--- components/profile/Description.tsx
+++ components/profile/Description.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Description = ({ description }: { description: string }) => {
   return (
     <div className="mt-4 w-3/4 flex h-auto items-start justify-center py-2 break-keep">
-      <p className="text-lg text-wave-300">{description}</p>
+      <p className="text-lg text-wave-300 text-center">{description}</p>
     </div>
   );
 };
