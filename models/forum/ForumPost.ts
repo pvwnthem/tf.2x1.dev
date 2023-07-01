9f085ed33fc27740e6df8a96515abbc054f39f4b
pvwnthem <pvwn.them@gmail.com>
2023-07-01 01:48:03 +0000

    allow for post to be used in a thread

--- models/forum/ForumPost.ts
+++ models/forum/ForumPost.ts
@@ -1,14 +1,22 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
 interface IForumPost extends Document {
+  postId: string;
   title: string;
   content: string;
   author: string;
+  parent: string | null;
+  replies: string[];
   createdAt: Date;
   updatedAt: Date;
 }
 
 const ForumPostSchema: Schema = new Schema({
+  postId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   title: {
     type: String,
     required: true,
@@ -21,6 +29,15 @@ const ForumPostSchema: Schema = new Schema({
     type: String,
     required: true,
   },
+  parent: {
+    type: String,
+    ref: 'ForumPost',
+    default: null,
+  },
+  replies: [{
+    type: String,
+    ref: 'ForumPost',
+  }],
   createdAt: {
     type: Date,
     default: Date.now,
@@ -29,6 +46,10 @@ const ForumPostSchema: Schema = new Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+ForumPostSchema.set('toJSON', {
+  virtuals: true,
 });
 
 const ForumPost = mongoose.models.ForumPost || mongoose.model<IForumPost>('ForumPost', ForumPostSchema);
