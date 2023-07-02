3e1d25c3c456d5784f1fb7dc3eed5809716c7927
pvwnthem <pvwn.them@gmail.com>
2023-07-02 23:52:06 +0000

    start post page

--- app/forum/posts/[postId]/page.tsx
+++ app/forum/posts/[postId]/page.tsx
@@ -1,39 +1,84 @@
 'use client'
 import UserNotFound from "@components/auth/errors/UserNotFound";
+import Badge from "@components/levels/Badge";
+import DeletedBadge from "@components/levels/DeletedBadge";
+import { Navbar } from "@components/navigation/navbar";
+import Loading from "@components/pages/loading";
+import { deletedUserPfp } from "@constants/images";
 import { getPost } from "@services/forum.service";
+import { getUser } from "@services/users.service";
 import { useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react"
 
-export default function PostPage ( { params } : any ) {
+export default function PostPage({ params }: any) {
+  const [post, setPost] = useState<any>(null);
+  const [user, setUser] = useState<any>(null);
+  const [notFound, setNotFound] = useState<boolean>(false);
+  const [postLoading, setPostLoading] = useState<boolean>(true);
+  const [userLoading, setUserLoading] = useState<boolean>(true);
+  const session = useSession();
 
-    const [post, setPost] = useState<any>(null)
-    const [notFound, setNotFound] = useState<boolean>(false)
-    const session = useSession();
+  useEffect(() => {
+    async function getData() {
+      if (params.postId) {
+        setPostLoading(true);
+        setUserLoading(true);
 
-    useEffect(() => {
-        async function getData() {
-            if (params.postId) {
-                const post = await getPost(params.postId);
-                console.log(post, params.postId)
+        const post = await getPost(params.postId);
+        console.log(post, params.postId)
 
-                if (post) {
-                    setPost(post)
-                } else {
-                    setNotFound(true);
-                }
-            }
+        if (post) {
+          setPost(post);
+          const user = await getUser(post.author);
+          setUser(user);
+          setUserLoading(false);
+        } else {
+          setNotFound(true);
         }
 
-        getData();
-    }, [params.user, params.category]);
-
-    if (notFound) {
-        return <UserNotFound />;
+        setPostLoading(false);
+      }
     }
 
-    return (
-        <>
-            <div></div>
-        </>
-    )
+    getData();
+  }, [params.postId, params.category]);
+
+  if (notFound) {
+    return <UserNotFound />;
+  }
+
+  if (postLoading || userLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <Navbar overlapsNot={true} />
+      <div className="w-full h-screen md:px-16 bg-background flex flex-col items-center">
+
+        <div className="w-full border flex">
+          <div className="w-1/5 flex flex-col border p-8 items-center justify-center">
+            <img
+              src={user ? user.profilePicture : deletedUserPfp}
+              alt="Profile Picture"
+              className="w-1/2 px-2 mt-4"
+            />
+            <h1 className="text-wave-400 text-3xl mt-4">{user ? user.username : "deleted user"}</h1>
+            <h1 className="mt-1 text-wave-200">created { user ? (new Date(user.createdAt).toLocaleDateString('en-US', {month: '2-digit', day: '2-digit', year: 'numeric'})) : "00/00/0000"}{' '} at {user ? (new Date(user.createdAt).toLocaleTimeString('en-US')) : "00:00:00"}</h1>
+            <div className="mt-2 flex flex-col items-center justify-center">
+                { user ? (
+                <Badge user={user} />
+            ) : <DeletedBadge /> }
+            </div>
+            
+
+          </div>
+
+          <h1>{post.title}</h1>
+
+        </div>
+
+      </div>
+    </>
+  );
 }
