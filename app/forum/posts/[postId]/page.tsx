5d5d8d99b428a207857cee2031bb0fa4ddefaea0
pvwnthem <pvwn.them@gmail.com>
2023-07-03 00:56:53 +0000

    revert work on replies

--- app/forum/posts/[postId]/page.tsx
+++ app/forum/posts/[postId]/page.tsx
@@ -43,11 +43,9 @@ export default function PostPage({ params }: any) {
     getData();
   }, [params.postId, params.category]);
 
-  const handleReply = () => {
-  };
+  const handleReply = () => {};
 
-  const handleLike = () => {
-  };
+  const handleLike = () => {};
 
   if (notFound) {
     return <UserNotFound />;
@@ -61,51 +59,55 @@ export default function PostPage({ params }: any) {
     <>
       <Navbar overlapsNot={true} />
       <div className="w-full min-h-screen md:px-16 bg-background flex flex-col items-center">
-        <div className="w-full border flex p-8">
-          <div className="w-1/5 flex flex-col border rounded p-8 items-center justify-center">
-            <img
-              src={user ? user.profilePicture : deletedUserPfp}
-              alt="Profile Picture"
-              className="w-1/2 px-2 mt-4"
-            />
-            <h1 className="text-wave-400 text-3xl mt-4">
-              {user ? user.username : "deleted user"}
-            </h1>
-            <h1 className="mt-1 text-sm text-wave-200">
-              created{" "}
-              {user
-                ? new Date(user.createdAt).toLocaleDateString("en-US", {
-                    month: "2-digit",
-                    day: "2-digit",
-                    year: "numeric",
-                  })
-                : "00/00/0000"}{" "}
-              at{" "}
-              {user
-                ? new Date(user.createdAt).toLocaleTimeString("en-US")
-                : "00:00:00"}
-            </h1>
-            <div className="mt-2 flex flex-col items-center justify-center">
-              {user ? <Badge user={user} /> : <DeletedBadge />}
+        <div className="w-full border flex-col p-8">
+          <div className="flex w-full">
+            <div className="w-1/5 flex flex-col border rounded p-8 items-center justify-center">
+              <img
+                src={user ? user.profilePicture : deletedUserPfp}
+                alt="Profile Picture"
+                className="w-1/2 px-2 mt-4"
+              />
+              <h1 className="text-wave-400 text-3xl mt-4">
+                {user ? user.username : "Deleted User"}
+              </h1>
+              <h1 className="mt-1 text-sm text-wave-200">
+                Created{" "}
+                {user
+                  ? new Date(user.createdAt).toLocaleDateString("en-US", {
+                      month: "2-digit",
+                      day: "2-digit",
+                      year: "numeric",
+                    })
+                  : "00/00/0000"}{" "}
+                at{" "}
+                {user
+                  ? new Date(user.createdAt).toLocaleTimeString("en-US")
+                  : "00:00:00"}
+              </h1>
+              <div className="mt-2 flex flex-col items-center justify-center">
+                {user ? <Badge user={user} /> : <DeletedBadge />}
+              </div>
             </div>
-          </div>
 
-          <div className="px-12">
-            <h1 className="text-wave-300 text-3xl">{post.title}</h1>
-            <p className="text-wave-400 mt-8">{post.content}</p>
+            <div className="px-12">
+              <div className="post-content">
+                <h1 className="text-wave-300 text-3xl">{post.title}</h1>
+                <p className="text-wave-400 mt-8">{post.content}</p>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="flex justify-between w-full mt-8">
+        <div className="w-full flex justify-end px-8 mt-4 space-x-4">
           <button
             onClick={handleReply}
-            className="bg-wave-200 hover:bg-primary-dark text-white font-semibold py-2 px-4 rounded"
+            className="bg-wave-500 hover:bg-wave-400 px-8 py-4 rounded text-white flex whitespace-nowrap text-xl"
           >
             Reply
           </button>
           <button
             onClick={handleLike}
-            className="bg-wave-400 hover:bg-secondary-dark text-white font-semibold py-2 px-4 rounded"
+            className="bg-wave-300 hover:bg-wave-400 px-8 py-4 rounded text-white flex whitespace-nowrap text-xl"
           >
             Like
           </button>
