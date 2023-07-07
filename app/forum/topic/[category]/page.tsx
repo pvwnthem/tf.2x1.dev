322f8d18035b5c5915e6765ad9aabc75047293f4
pvwnthem <pvwn.them@gmail.com>
2023-07-07 22:29:20 +0000

    unused imports

--- app/forum/topic/[category]/page.tsx
+++ app/forum/topic/[category]/page.tsx
@@ -3,13 +3,9 @@
 'use client'
 import React, { useState, useEffect } from 'react'
 
-import { IUser } from '@models/User'
-import { getUserByName } from '@services/users.service'
 import Loading from '@components/pages/loading'
 import { useSession } from 'next-auth/react'
 import UserNotFound from '@components/auth/errors/UserNotFound'
-import UserProfile from '@components/pages/userProfile'
-import BackButton from '@components/navigation/back'
 import { getAllPostsInCategory } from '@services/forum.service'
 import { IForumPost } from '@models/forum/ForumPost'
 import Post from '@components/forum/Post'
