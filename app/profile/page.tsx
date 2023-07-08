118ea9dd49dc21ca19bc5569dd487f5f57281bd1
pvwnthem <pvwn.them@gmail.com>
2023-07-08 15:33:03 -0400

    white text, use navbar on profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,7 +5,7 @@ import { useSession } from 'next-auth/react'
 import Protected from '@components/auth/protected'
 import Wrapper from '@components/auth/Wrapper'
 import ProfilePage from '@components/pages/profile'
-import BackButton from '@components/navigation/back'
+import { Navbar } from '@components/navigation/navbar'
 import EmailProtected from '@components/auth/emailProtected'
 
 export default function Profile() {
@@ -16,7 +16,7 @@ export default function Profile() {
             <Wrapper session={session}>
                 <Protected session={session}>
                     <EmailProtected session={session}>
-                        <BackButton />
+                        <Navbar />
                         <ProfilePage session={session} />
                     </EmailProtected>
                 </Protected>
